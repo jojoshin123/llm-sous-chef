96272270af658e0abd6c1e17b28cf7a234a299fa
jojoshin123 <jojoshin123@gmail.com>
2025-04-10 19:16:52 -0400

    init psycopg and ddl

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    username TEXT NOT NULL,
+    hashed_pwd TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS recipes (
+    id TEXT PRIMARY KEY, -- primary key will be URL?
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    space_id INTEGER REFERENCES spaces(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS spaces (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS space_user_map (
+    PRIMARY KEY (user_id, space_id),
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    space_id INTEGER NOT NULL REFERENCES spaces(id)
+);
