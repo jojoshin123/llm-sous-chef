a2a46bbaeeb87f765082bea000a0c00761277a91
jojoshin123 <jojoshin123@gmail.com>
2025-04-10 22:41:00 -0400

    add require_auth decorator

--- llm_sous_chef/auth.py
+++ llm_sous_chef/auth.py
@@ -1,15 +1,35 @@
 import os
+from functools import wraps
 import jwt
 from datetime import datetime, timedelta
+from flask import request, jsonify
 
 EXPIRATION_MINUTES = 120
+SECRET_KEY = os.getenv("JWT_SECRET")
 
 def create_jwt(data: dict):
     data_copy = data.copy() # To add "expire" field for jwt
-    secret = os.getenv("JWT_SECRET")
     data_copy["exp"] = datetime.utcnow() + timedelta(minutes=EXPIRATION_MINUTES)
 
-    return jwt.encode(data_copy, secret, algorithm="HS256")
+    return jwt.encode(data_copy, SECRET_KEY, algorithm="HS256")
 
 # TODO: create @require_auth annotation for protected endpoints
 
+def require_auth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        auth_header = request.headers.get("Authorization", "")
+        if not auth_header.startswith("Bearer"):
+            return jsonify({"Error": "Invalid Bearer token"}), 401
+
+        token = auth_header.split(" ")[1]
+        try:
+            user_payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        except jwt.ExpiredSignatureError:
+            return jsonify({"Error": "Token expired"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid token"}), 401
+
+        # Attach user payload to request object
+        request.user = user_payload
+        return func(*args, **kwargs)
