43737f964dd2a9fe99b01f53c763a1af5f31ce62
jojoshin123 <jojoshin123@gmail.com>
2025-04-11 23:43:45 -0400

    change up prompt for more specificity

--- llm_sous_chef/get_recipe.py
+++ llm_sous_chef/get_recipe.py
@@ -8,14 +8,22 @@ def get_recipe(video_data):
         messages=[
             {
                 "role": "system",
-                "content": "You are an assistant tasked to summarize cooking recipes from social media content."
+                "content": """
+                    You are an assistant tasked to summarize cooking recipes from social media content.
+                    These recipes are meant for home cooking and should be relatively simple without sacrificing quality.
+                    """
             },
             # Set a user message for the assistant to respond to.
             {
                 "role": "user",
                 "content": """
-                Using this metadata and transcription of a cooking TikTok, please generate a recipe. 
-                Please ONLY respond with ingredients and instructions.
+                Using this metadata and transcription of a cooking TikTok, please generate a recipe.
+                I want you to return this recipe as a JSON object.
+                Please ONLY respond with:
+                1. A key "title" with a title string as a value.
+                2. A key "description" with an 2-3 brief description string as a value.
+                3. A key "ingredients" with a list of ingredients and quantity needed.
+                4. A key "instructions" with a list of detailed instructions.
                 """ + str(video_data),
             }
         ],
