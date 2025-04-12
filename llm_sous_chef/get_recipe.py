2c18af7b59fb10461c449744d0c30f01571da341
jojoshin123 <jojoshin123@gmail.com>
2025-04-12 00:06:14 -0400

    trying some other prompting

--- llm_sous_chef/get_recipe.py
+++ llm_sous_chef/get_recipe.py
@@ -18,12 +18,18 @@ def get_recipe(video_data):
                 "role": "user",
                 "content": """
                 Using this metadata and transcription of a cooking TikTok, please generate a recipe.
-                I want you to return this recipe as a JSON object.
-                Please ONLY respond with:
-                1. A key "title" with a title string as a value.
-                2. A key "description" with an 2-3 brief description string as a value.
-                3. A key "ingredients" with a list of ingredients and quantity needed.
-                4. A key "instructions" with a list of detailed instructions.
+                I want you to return this recipe as a JSON object in the format specified below.
+                {
+                    "description": "",
+                    "ingredients": [
+                        {
+                            "ingredient": "" (This should contain both the ingredient quantity and name.)
+                        }
+                    ],
+                    "instructions": [],
+                    "title": ""
+                }
+                
                 """ + str(video_data),
             }
         ],
