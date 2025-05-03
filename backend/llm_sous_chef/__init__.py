aa4077805baba3a80d119b5a9724ec871eac8810
jojoshin123 <jojoshin123@gmail.com>
2025-05-03 16:42:36 -0400

    origins env

--- backend/llm_sous_chef/__init__.py
+++ backend/llm_sous_chef/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -6,7 +8,8 @@ from flask_cors import CORS
 def create_app():
     load_dotenv()
     app = Flask(__name__)
-    CORS(app, origins=["http://localhost:3000"])
+    origins_env = os.getenv("ORIGINS")
+    CORS(app, origins=origins_env)
 
     from .routes import main
     app.register_blueprint(main)
